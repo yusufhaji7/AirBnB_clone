4b6200f3ae94f1c3116b437b6eabca6448b178d9
yusufhaji7 <yususfhadji873@gmail.com>
2022-11-22 09:48:54 -0800

    done with tasks until 4

--- models/base_model.py
+++ models/base_model.py
@@ -7,20 +7,34 @@ class BaseModel with public attributes
 
 
 class BaseModel:
+    """BaseModel class"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """initial method"""
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    v = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if k == "id":
+                    self.id = value
+                if k != "__class__":
+                    setattr(self, k, v)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
         string representation
         """
-        return("[{}] ({}) {}".format(self.__class__.__name__,
-            self.id, self.__dict__))
+        name = self.__class__.__name__
+        return("[{}] ({}) {}".format(name, self.id, self.__dict__))
 
     def save(self):
         """
@@ -30,7 +44,7 @@ class BaseModel:
 
     def to_dict(self):
         """
-        returns a dictionary containing all keys/values 
+        returns a dictionary containing all keys/values
         of __dict__ of the instance
         """
         dictio = self.__dict__.copy()
